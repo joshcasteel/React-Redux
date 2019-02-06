f75e08679f9dbb58d42709233e9ac949de656e0c
joshcasteel <joshcasteelz@gmail.com>
2019-02-06 13:13:38 -0500

    changed class component to functional component

--- src/ninja.js
+++ src/ninja.js
@@ -1,20 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Ninjas extends Component {
-  render() {
-    const { ninjas } = this.props;
-    const ninjaList = ninjas.map((ninja, i) => {
-      console.log(i);
-      return (
-        <div className="ninja" key={i}>
-          <div>Name: {ninja.name}</div>
-          <div>Age: {ninja.age}</div>
-          <div>Belt: {ninja.belt}</div>
-        </div>
-      );
-    });
-    return <div className="ninja-list">{ninjaList}</div>;
-  }
-}
+const Ninjas = ({ ninjas }) => {
+  const ninjaList = ninjas.map((ninja, i) => {
+    console.log(i);
+    return (
+      <div className="ninja" key={i}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>Belt: {ninja.belt}</div>
+      </div>
+    );
+  });
+  return <div className="ninja-list">{ninjaList}</div>;
+};
 
 export default Ninjas;
